85d048421278b08b67e3c1a601d03856e35255e9
guilherme-marcello <guilemosm@gmail.com>
2023-10-17 12:40:23 +0100

    Implemented UAV handover

--- uas.py
+++ uas.py
@@ -24,6 +24,47 @@ class UAV(DemandConsumer, InterconnectedDevice):
         total = self.get_serving_zone_demand() + demand_from_incoming_devices
         #print(f"Total demand for UAV {self.name} is {total} ({self.get_serving_zone_demand()} + {demand_from_incoming_devices})")
         return total
+    
+    def handover(self, other):
+        source_zone = self.serving_zone
+        self.serving_zone = other.serving_zone
+        other.serving_zone = source_zone
+
+        source_incoming_devices = []
+        for incoming_device in self.incoming_devices:
+            incoming_device: InterconnectedDevice = incoming_device
+            self.remove_incoming_device(incoming_device)
+            source_incoming_devices.append(incoming_device)
+
+        sink_incoming_devices = []
+        for incoming_device in other.incoming_devices:
+            incoming_device: InterconnectedDevice = incoming_device
+            other.remove_incoming_device(incoming_device)
+            sink_incoming_devices.append(incoming_device)
+
+        # update gateways
+        source_gateway = self.outgoing_device
+        if source_gateway:
+            source_gateway.remove_incoming_device(self)
+            source_gateway.add_incoming_device(other)
+
+        sink_gateway = other.outgoing_device
+        if sink_gateway:
+            sink_gateway.remove_incoming_device(other)
+            sink_gateway.add_incoming_device(self)
+        
+        self.outgoing_device = other.outgoing_device
+        other.outgoing_device = source_gateway
+
+        # update incoming devices
+        for device in sink_incoming_devices:
+            device: InterconnectedDevice = device
+            self.add_incoming_device(device)
+            device.set_outgoing_device(self)
+        for device in source_incoming_devices:
+            device: InterconnectedDevice = device
+            other.add_incoming_device(device)
+            device.set_outgoing_device(other)
 
     def __repr__(self) -> str:
         string = f"UAV {self.name} "
